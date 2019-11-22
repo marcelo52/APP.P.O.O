fb4b1bf194f4aa20e9417fe542f55d2f92ddb453
marcelo52 <marcelo.13maia@outlook.com.br>
2019-11-21 21:53:42 -0300

    upadate

--- app/src/main/java/com/example/apppoo/SegundaTela.java
+++ app/src/main/java/com/example/apppoo/SegundaTela.java
@@ -3,6 +3,12 @@ package com.example.apppoo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 
 public class SegundaTela extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class SegundaTela extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela);
+    }
+
+    public void cadastrarUsuario(View v) throws IOException {
+        EditText campoEmail = findViewById(R.id.emailCampo2);
+        String email = campoEmail.getText().toString();
+        EditText campoNome = findViewById(R.id.nomeCampo);
+        String nome = campoNome.getText().toString();
+        EditText campoSenha = findViewById(R.id.senhaCampo2);
+        String senha = campoSenha.getText().toString();
+        EditText confirmSenha = findViewById(R.id.confirmSenhaCampo);
+        String confirmarSenha = confirmSenha.getText().toString();
+        int resposta = DBHelper.insertIntoUsuario(nome, email, senha);
+        if (resposta == 1) {
+            Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Erro! Cadastro não realizado!", Toast.LENGTH_LONG).show();
+        }
     }
 }
